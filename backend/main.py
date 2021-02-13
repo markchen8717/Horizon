11f17d8081d3e54df11bf36f313f1613bfbf38f3
Mark Chen <29904105+markchen8717@users.noreply.github.com>
2021-02-13 16:18:32 -0500

    REST API Backbone;

--- backend/main.py
+++ backend/main.py
@@ -6,30 +6,61 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+'''
+Session
+    - uuid
+    - questions
+    - users
+
+User
+    - uuid
+    - uuid of the group they belong to
+
+Group
+    - uuid
+    - user list
+'''
+
 #Create
 
 #Create a new session and return session uuid
 @app.route('/sessions', methods=['POST'])
-def newSession():
+def addSession():
     pass
 
 #Create a new user for a given session
 @app.route('/sessions/:sessionID/users', methods=['POST'])
-def newUser():
+def addUser():
     pass
 
 #Create a response for a given user for a given session
-@app.route('/sessions/:sessionID/users/userID/responses', methods=['PUT'])#PUT is idempotent, POST is not
-def newResponse():
+@app.route('/sessions/:sessionID/users/userID/responses', methods=['POST'])
+def createResponse():
     pass
 
 #Read
 
 #Get an ordered list of questions for a given session
 @app.route('/sessions/:id/questions', methods=['GET'])
-def index():
+def getSessionQuestions():
     pass
 
+#Get info about a group
+@app.route('/groups/:id', methods=['GET'])
+def getGroup():
+    pass
+
+#Get info about a user
+@app.route('/users/:id', methods=['GET'])
+def getUser():
+    pass
+
+#Update
+
+#Update a user
+@app.route('/users/:id',methods=['PUT'])
+def updateUser():
+    pass
    
 
 if __name__ == '__main__':
