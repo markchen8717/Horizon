14d24da7240593a906a2a413449a3fd1707cd2a5
removedporn <86824510+removedporn@users.noreply.github.com>
2023-08-25 17:21:31 +0800

    Delete backend directory

--- backend/User.py
+++
@@ -1,9 +0,0 @@
-class User:
-    def __init__(self, uuid, username):
-        self.uuid = uuid
-        self.group = None
-        self.responses = None
-        self.username = username
-    
-    def setResponses(self,responses):
-        self.responses = responses
